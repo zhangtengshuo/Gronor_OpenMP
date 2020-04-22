7562c9dff33d1883501cea74f1cd66db2e0d215c
T.P.Straatsma <mailb@tpstraatsma.com>
2020-04-22 10:32:11 -0400

    Update test script

--- scripts/test.py
+++ scripts/test.py
@@ -3,23 +3,19 @@
 import os
 import sys
 
-num = len(sys.argv)
-
-print('PYTHON SCRIPT num is ',num,' ',sys.argv[1])
-
 name = sys.argv[1]
 
-comm = 'ls -al ' + name + '.inp'
+runfile = name+".run"
 
-print('Comm is ',comm)
+runcommand = open(runfile).read()
 
-os.system(comm)
-os.system('ls -al '+name+'.inp')
-
-os.system('mpirun -n 7 gronor '+name)
+os.system(runcommand+' '+name)
 
 os.system('diff '+name+'.tst '+name+'.ok > '+name+'.dif')
 
 len = os.stat(name+'.dif').st_size
 
-print('Diff size is ',len)
+if (len = 0) sys.exit(0)
+
+sys.exit(1)
+

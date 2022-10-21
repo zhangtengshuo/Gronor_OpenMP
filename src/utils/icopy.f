4cce768a1dbc93ce6d118679235daaf34a0df4f4
T. P. Straatsma <mailb@tpstraatsma.com>
2022-10-21 15:37:08 -0400

    Add continuation lines to support f77 and f90 compilers

--- src/utils/icopy.f
+++ src/utils/icopy.f
@@ -1,11 +1,11 @@
 !> @brief Strided copy routine
 !> @author Tjerk P. Straatsma
 
-      
+
       Subroutine icopy(n,src,ix,dest,iy)
-*
-* $Id: icopy.f 19707 2010-10-29 17:59:36Z d3y133 $
-*
+!
+! $Id: icopy.f 19707 2010-10-29 17:59:36Z d3y133 $
+!
       implicit none
       integer n, src(*), ix, dest(*), iy
       integer i,ii,jj

1b1cd857b2963c0c70850e5cc78c2286b82ad021
Aitor Sanchez <aitorsanchezmansilla@gmail.com>
2022-11-08 14:51:24 +0100

    changed rdtraint to read integrals

--- build/CMakeFiles/inp.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "CMakeFiles/mod/inp.mod"
-  "CMakeFiles/mod/INP.mod"
-  "CMakeFiles/inp.dir/inp.mod.stamp"
-  )
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/qq/mansilla/master
-# Build directory: /home/qq/mansilla/master/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

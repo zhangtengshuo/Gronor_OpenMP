2a3f57d1b30f8d2d4afffe77cc8078b9f9b87511
T. P. Straatsma <mailb@tpstraatsma.com>
2023-05-05 17:02:19 -0400

    Add new cofac1 evd and svd files

---
+++ src/gnome/gronor_cofac1.F90
@@ -0,0 +1,557 @@
+
+!     This file is part of the GronOR software
+
+!     GronOR is free software, and can be used, re-distributed and/or modified under
+!     the Apache License version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+!     Any use of the software has to be in compliance with this license. Unless required
+!     by applicable law or agreed to in writing, software distributed under the license
+!     is distributed on an ‘as is’ bases, without warranties or conditions of any kind,
+!     either express or implied.
+!     See the license for the specific language governing permissions and limitations
+!     under the license.
+
+!     GronOR is copyright of the University of Groningen
+
+!> @brief
+!! Cofactor matrix evaluation and factorization
+!!
+!! @author  R. Broer, RUG
+!! @author  T. P. Straatsma, ORNL
+!! @date    2016
+!!
+
+      subroutine gronor_cofac1(lfndbg)
+      use cidist
+      use gnome_parameters
+      use gnome_data
+      use gnome_solvers
+      
+      implicit none
+      integer :: lfndbg
+      integer :: i,ierr,j,idetuw,k
+      real (kind=8) :: coef
+      real (kind=8) :: cmax, cnorm, coefu
+      integer :: nz1, nz2
+
+
+      if(idbg.ge.30) write(lfndbg,600)
+ 600  format(/,' Cofactor matrix will be calculated')
+
+
+      if(idbg.ge.90) then
+#ifdef ACC
+!$acc update host (a)
+#endif
+        write(lfndbg,1601) nelecs,nelecs,mbasel
+ 1601   format(//,' SVD input matrix:',3i6,/)
+        do j=1,nelecs
+          write(lfndbg,1602) (a(i,j),i=1,nelecs)
+ 1602     format((3x,6e20.12))
+        enddo
+        flush(lfndbg)
+      endif
+
+      call timer_start(41)
+      
+      call gronor_svd(lfndbg)
+
+      call timer_stop(41)
+      
+#ifdef ACC
+!$acc kernels present(u,w,a)
+#endif
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp target teams loop
+#else
+!$omp target teams distribute parallel do
+#endif
+!$omp& private(coef,k)
+#endif
+      do i=1,nelecs
+        do j=1,i
+          coef=0.0d0
+          do k=1,nelecs
+            coef=coef+u(i,k)*w(k,j)+u(j,k)*w(k,i)
+          enddo
+          a(i,j)=coef
+          a(j,i)=coef
+        enddo
+      enddo
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp end target teams loop
+#else
+!$omp end target teams distribute parallel do
+#endif
+#endif
+#ifdef ACC
+!$acc end kernels
+#endif
+      
+      if(idbg.ge.90) then
+#ifdef ACC
+!$acc update host (u,w,a,ev)
+#endif
+#ifdef OMPTGT 
+!$omp target update from(a,ev,u,w)
+#endif  
+        write(lfndbg,601) (ev(i),i=1,nelecs)
+ 601    format(//,' Eigenvalues of diagonalized overlap matrix:',               &
+     &       //,(3x,6e20.12))
+        write(lfndbg,1603) nelecs,nelecs,mbasel
+ 1603   format(//,' Matrix u:',3i6,/)
+        do j=1,nelecs
+          write(lfndbg,1602) (u(i,j),i=1,nelecs)
+        enddo
+        write(lfndbg,1604) nelecs,nelecs,mbasel
+ 1604   format(//,' Matrix w:',3i6,/)
+        do j=1,nelecs
+          write(lfndbg,1602) (w(i,j),i=1,nelecs)
+        enddo
+        write(lfndbg,1605) nelecs,nelecs,mbasel
+ 1605   format(//,' Coefficient matrix:',3i6,/)
+        do j=1,nelecs
+          write(lfndbg,1602) (a(i,j),i=1,nelecs)
+        enddo
+        flush(lfndbg)
+      endif
+
+!     Calculation of det(uw) , by determination of the number
+!     of eigenvalues -2 of " a=uw+tranposed(uw) "
+
+      idetuw=1
+
+      call timer_start(43)
+      
+      call gronor_evd(lfndbg)
+
+      call timer_stop(43)
+      
+      call timer_start(44)
+
+      cmax=0.0d0
+
+#ifdef ACC
+!$acc kernels present(cdiag,diag,csdiag,sdiag,ev)
+#endif
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp target teams loop
+#else
+!$omp target teams distribute parallel do
+#endif
+!$omp& reduction(max:cmax)
+#endif
+!     calculation of det(a) and x and y
+      do i=1,nelecs
+        if(diag(i).lt.-1.999999d0) idetuw=-idetuw
+        if(abs(ev(i)).gt.cmax) cmax=abs(ev(i))
+        cdiag(i)=diag(i)
+        csdiag(i)=sdiag(i)
+      enddo
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp end target teams loop
+#else
+!$omp end target teams distribute parallel do
+#endif
+#endif
+
+#ifdef ACC
+!$acc end kernels
+#endif
+!cc   !_OMPTGT_($omp end target)
+      if(cmax.le.0.01) call gronor_abort(310,"No overlap between m.o.s")
+
+      if(idbg.ge.90) then
+#ifdef ACC
+!$acc update host (diag,cdiag,csdiag,sdiag)
+#endif
+        write(lfndbg,604)                                                       &
+     &       (diag(i),cdiag(i),csdiag(i),sdiag(i),i=1,nelecs)
+ 604    format(//,' Diagonals:',                                                &
+     &       //,(3x,4e20.12))
+        flush(lfndbg)
+      endif
+
+      cnorm=tau_SIN*cmax
+      coef=idetuw
+      nz1=0
+      nz2=0
+      deta=0.0d0
+
+#ifdef ACC
+!$acc update host(ev)
+#endif
+#ifdef OMPTGT
+!$omp target update from(ev)
+#endif
+      do i=1,nelecs
+        coefu=ev(i)
+        if(abs(coefu) .le. cnorm) then
+          if(nz1.gt.0.and.nz2.gt.0) then
+            ising=3
+            call timer_stop(44)
+            return
+          endif
+          if(nz1 .gt. 0) nz2=i
+          if(nz1.eq.0) nz1=i
+        else
+          coef=coef*coefu
+        endif
+      enddo
+
+      ising=2
+      if(nz2.le.0) then
+        ising=1
+        if(nz1.le.0) then
+          deta=coef
+          ising=0
+
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp target teams loop
+#else
+!$omp target teams distribute parallel do
+#endif
+!$omp& collapse(2) private(coefu)
+#endif
+#ifdef ACC
+!$acc parallel loop gang collapse(2) present(ta,u,w,ev)
+#endif
+          do i=1,nelecs
+            do j=1,nelecs
+              coefu=0.0d0
+              do k=1,nelecs
+                coefu=coefu+u(i,k)*w(j,k)/ev(k)
+              enddo
+              ta(i,j)=coefu*0.5d0
+            enddo
+          enddo
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp end target teams loop
+#else
+!$omp end target teams distribute parallel do
+#endif
+#endif
+#ifdef ACC
+!$acc end parallel loop
+#endif
+          call timer_stop(44)
+          return
+          ising=1
+        endif
+
+#ifdef ACC
+!$acc kernels present(ta,u,w,ev,cdiag,diag,csdiag,sdiag)
+#endif
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp target teams loop
+#else
+!$omp target teams distribute parallel do
+#endif
+#endif
+        do i=1,nelecs
+          diag(i)=u(i,nz1)*coef
+          sdiag(i)=w(i,nz1)
+          cdiag(i)=diag(i)
+          csdiag(i)=sdiag(i)
+        enddo
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp end target teams loop
+#else
+!$omp end target teams distribute parallel do
+#endif
+#endif
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp target teams loop
+#else
+!$omp target teams distribute parallel do
+#endif
+!$omp& collapse(2) private(coefu)
+#endif
+        do i=1,nelecs
+          do j=1,nelecs
+            coefu=0.0d0
+            do k=1,nelecs
+              if(k.ne.nz1) coefu=coefu+u(i,k)*w(j,k)/ev(k)
+            enddo
+            ta(i,j)=coefu
+          enddo
+        enddo
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp end target teams loop
+#else
+!$omp end target teams distribute parallel do
+#endif
+#endif
+#ifdef ACC
+!$acc end kernels
+#endif
+        call timer_stop(44)
+        return
+      endif
+
+      if(abs(coef).lt.tau_SIN) then
+        ising=3
+      else
+#ifdef ACC
+!$acc kernels present(cdiag,diag,csdiag,sdiag,u,w,ta)
+#endif
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp target teams loop
+#else
+!$omp target teams distribute parallel do
+#endif
+!$omp& private(coefu)
+#endif
+        do i=1,nelecs
+          diag(i)=u(i,nz1)*coef
+          sdiag(i)=w(i,nz1)
+          cdiag(i)=diag(i)
+          csdiag(i)=sdiag(i)
+          coefu=u(i,nz2)
+          do j=1,nelecs
+            ta(i,j)=coefu*w(j,nz2)
+          enddo
+        enddo
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp end target teams loop
+#else
+!$omp end target teams distribute parallel do
+#endif
+#endif
+!c!_OMPTGT_($omp target teams distribute parallel do)
+!c        do i=1,nelecs
+!c          cdiag(i)=diag(i)
+!c          csdiag(i)=sdiag(i)
+!c        enddo
+!cc!_OMPTGT_($omp end target)
+#ifdef ACC
+!$acc end kernels
+#endif
+        call timer_stop(44)
+        return
+      endif
+
+      call timer_stop(44)
+
+      return
+      end
+
+      subroutine gronor_cofac1_omp(lfndbg)
+      use cidist
+      use gnome_parameters
+      use gnome_data
+      use gnome_solvers
+#ifdef MKL
+      use mkl_solver
+#endif
+
+      implicit none
+      integer :: lfndbg
+      integer :: i,ierr,j,idetuw,k
+      real (kind=8) :: coef
+      real (kind=8) :: cmax, cnorm, coefu
+      integer :: nz1, nz2
+
+      if(idbg.ge.30) write(lfndbg,600)
+ 600  format(/,' Cofactor matrix will be calculated')
+
+      call timer_start(41)
+
+      if(idbg.ge.90) then
+        write(lfndbg,1601) nelecs,nelecs,mbasel
+ 1601   format(//,' SVD input matrix:',3i6,/)
+        do j=1,nelecs
+          write(lfndbg,1602) (a(i,j),i=1,nelecs)
+ 1602     format((3x,6e20.12))
+        enddo
+      endif
+
+call gronor_svd_omp(lfndbg)
+            
+      call timer_start(42)
+
+#ifdef OMP
+!$omp parallel do shared(a,u,w) private(coef)
+#endif
+      do i=1,nelecs
+        do j=1,i
+          coef=0.0d0
+          do k=1,nelecs
+            coef=coef+u(i,k)*w(k,j)+u(j,k)*w(k,i)
+          enddo
+          a(i,j)=coef
+        enddo
+      enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+      call timer_stop(42)
+
+      call timer_stop(41)
+
+      if(idbg.ge.90) write(lfndbg,601) (ev(i),i=1,nelecs)
+ 601  format(//,' Eigenvalues of diagonalized overlap matrix:',                 &
+     & //,(3x,6e20.12))
+
+!     Calculation of det(uw) , by determination of the number
+!     of eigenvalues -2 of " a=uw+tranposed(uw) "
+
+      idetuw=1
+
+      call timer_start(43)
+
+      call gronor_evd_omp(lfndbg)
+
+      call timer_stop(43)
+
+      call timer_start(44)
+      do i=1,nelecs
+        if(diag(i).lt.-1.999999d0) idetuw=-idetuw
+      enddo
+
+#ifdef OMP
+!$omp parallel do shared(cdiag,csdiag,diag,sdiag)
+#endif
+      do i=1,nelecs
+        cdiag(i)=diag(i)
+        csdiag(i)=sdiag(i)
+      enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+!      calculation of det(a) and x and y
+
+      cmax=0.00
+
+      do i=1,nelecs
+        if(abs(ev(i)).gt.cmax) cmax=abs(ev(i))
+      enddo
+
+      if(cmax.le.0.01) call gronor_abort(310,"No overlap between m.o.s")
+
+      cnorm=tau_SIN*cmax
+      coef=idetuw
+      nz1=0
+      nz2=0
+      deta=0.0
+
+      do i=1,nelecs
+        coefu=ev(i)
+        if(abs(coefu) .le. cnorm) then
+          if(nz1.gt.0.and.nz2.gt.0) then
+            ising=3
+            call timer_stop(44)
+            return
+          endif
+          if(nz1 .gt. 0) nz2=i
+          if(nz1.eq.0) nz1=i
+        else
+          coef=coef*coefu
+        endif
+      enddo
+
+      ising=2
+      if(nz2.le.0) then
+        ising=1
+        if(nz1.le.0) then
+          deta=coef
+          ising=0
+
+#ifdef OMP
+!$omp parallel do shared(u,w,ev,ta) private(coefu) collapse(2)
+#endif
+          do i=1,nelecs
+            do j=1,nelecs
+              coefu=0.0d0
+              do k=1,nelecs
+                coefu=coefu+u(i,k)*w(j,k)/ev(k)
+              enddo
+              ta(i,j)=coefu*0.5d0
+            enddo
+          enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+          call timer_stop(44)
+          return
+          ising=1
+        endif
+
+#ifdef OMP
+!$omp parallel do shared(u,w,ev,ta,diag,sdiag) private(coefu)
+#endif
+        do i=1,nelecs
+          diag(i)=u(i,nz1)*coef
+          sdiag(i)=w(i,nz1)
+          do j=1,nelecs
+            coefu=0.0d0
+            do k=1,nelecs
+              if(k.ne.nz1) coefu=coefu+u(i,k)*w(j,k)/ev(k)
+            enddo
+            ta(i,j)=coefu
+          enddo
+        enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+#ifdef OMP
+!$omp parallel do shared(cdiag,csdiag,diag,sdiag)
+#endif
+        do i=1,nelecs
+          cdiag(i)=diag(i)
+          csdiag(i)=sdiag(i)
+        enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+        call timer_stop(44)
+        return
+      endif
+
+      if(abs(coef).lt.tau_SIN) then
+        ising=3
+      else
+
+#ifdef OMP
+!$omp parallel do shared(u,w,ta,diag,sdiag,coef) private(coefu)
+#endif
+        do i=1,nelecs
+          diag(i)=u(i,nz1)*coef
+          sdiag(i)=w(i,nz1)
+          coefu=u(i,nz2)
+          do j=1,nelecs
+            ta(i,j)=coefu*w(j,nz2)
+          enddo
+        enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+#ifdef OMP
+!$omp parallel do shared(cdiag,csdiag,diag,sdiag)
+#endif
+        do i=1,nelecs
+          cdiag(i)=diag(i)
+          csdiag(i)=sdiag(i)
+        enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+        call timer_stop(44)
+        return
+      endif
+
+      call timer_stop(44)
+
+      return
+      end
---
+++ src/gnome/gronor_evd.F90
@@ -0,0 +1,280 @@
+
+!     This file is part of the GronOR software
+
+!     GronOR is free software, and can be used, re-distributed and/or modified under
+!     the Apache License version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+!     Any use of the software has to be in compliance with this license. Unless required
+!     by applicable law or agreed to in writing, software distributed under the license
+!     is distributed on an ‘as is’ bases, without warranties or conditions of any kind,
+!     either express or implied.
+!     See the license for the specific language governing permissions and limitations
+!     under the license.
+
+!     GronOR is copyright of the University of Groningen
+
+!> @brief
+!! Cofactor matrix evaluation and factorization
+!!
+!! @author  R. Broer, RUG
+!! @author  T. P. Straatsma, ORNL
+!! @date    2016
+!!
+
+subroutine gronor_evd(lfndbg)
+  use cidist
+  use gnome_parameters
+  use gnome_data
+  use gnome_solvers
+
+  ! library specific modules
+
+#ifdef MKL
+  use mkl_solver
+#endif
+
+#ifdef CUSOLVER
+  use cusolverDn
+  use cuda_cusolver
+  use cudafor
+#endif
+
+  ! variable declarations
+
+  implicit none
+  integer :: lfndbg
+  integer :: i,j
+  integer :: ierr
+
+  ! library specific declarations
+
+#ifdef CUSOLVER
+  character (len=1), target :: jobu, jobvt
+#endif
+  
+  ! ========== EISPACK =========
+
+  if(jsolver.eq.SOLVER_EISPACK) then
+#ifdef ACC
+!$acc kernels present(sdiag)
+#endif
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp target teams loop
+#else
+!$omp target teams distribute parallel do
+#endif
+#endif
+      do i=1,nelecs
+        sdiag(i)=0.0d0
+      enddo
+#ifdef ACC
+!$acc end kernels
+#endif
+#ifdef OMPTGT
+#ifdef OMP5
+!$omp end target teams loop
+#else
+!$omp end target teams distribute parallel do
+#endif
+#endif
+    if(iamacc.eq.1) then
+#ifdef ACC
+!$acc update host (a)
+#endif
+#ifdef OMPTGT
+!$omp target update from(a)
+#endif    
+    endif
+    call tred2(nelecs,nelecs,a,nelecs,nelecs,diag, &
+        nelecs,sdiag,nelecs,a,nelecs,nelecs)
+    call tql2(nelecs,nelecs,diag,nelecs,sdiag,nelecs, &
+        a,nelecs,nelecs,ierr)
+    if(iamacc.eq.1) then
+#ifdef ACC
+!$acc update device (ev,u,w)
+#endif
+#ifdef OMPTGT
+!$omp target update to(ev,u,w)
+#endif
+    endif
+  endif
+  
+  ! ============ MKL ===========
+#ifdef MKL
+  if(jsolver.eq.SOLVER_MKL) then
+    if(iamacc.eq.1) then
+#ifdef ACC
+!$acc update host (a)
+#endif
+#ifdef OMPTGT
+!$omp target update from(a)
+#endif    
+    endif
+    ndimm=nelecs
+    call dsyevd('N','L',ndimm,a,nelecs,diag,workspace_d,lwork1m,workspace_i,lworki,ierr)
+    if(iamacc.eq.1) then
+#ifdef ACC
+      !$acc update device (ev,u,w)
+#endif
+#ifdef OMPTGT
+!$omp target update to(ev,u,w)
+#endif
+    endif
+  endif
+#endif 
+  
+  ! ========= CUSOLVER =========
+
+#ifdef CUSOLVER
+  if(jsolver.eq.SOLVER_CUSOLVER) then
+    ndim=nelecs
+    mdim=mbasel
+    jobu = 'A'  ! all m columns of U
+    jobvt= 'A'  ! all m columns of VT
+#ifdef ACC
+!$acc data copy(dev_info_d) create(workspace_d)
+!$acc wait
+!$acc host_data use_device(a,diag,dev_info_d,workspace_d)
+#endif
+#ifdef OMPTGT
+!$omp target data use_device_addr(a,ev,u,w,dev_info_d,workspace_d)
+#endif
+    cusolver_status = cusolverDnDsyevd(cusolver_handle, &
+        CUSOLVER_EIG_MODE_NOVECTOR,CUBLAS_FILL_MODE_LOWER, &
+        ndim,a,ndim,diag,workspace_d,lwork2,dev_info_d)
+#ifdef ACC
+!$acc end host_data
+!$acc wait
+#endif
+#ifdef OMPTGT
+!omp end target data
+#endif
+    if(cusolver_status /= CUSOLVER_STATUS_SUCCESS) &
+        write(*,*) 'cusolverDnDsyevd failed',cusolver_status
+  endif
+#endif
+  
+  ! ======== CUSOLVERJ =========
+
+#ifdef CUSOLVERJ  
+  if(jsolver.eq.SOLVER_CUSOLVERJ) then
+    ndim=nelecs
+    mdim=mbasel
+    jobz = CUSOLVER_EIG_MODE_NOVECTOR
+    uplo = CUBLAS_FILL_MODE_LOWER
+
+#ifdef ACC
+!$acc data copy(dev_info_d,syevj_params) create(workspace_d)
+!$acc host_data use_device(a,diag,dev_info_d,workspace_d)
+#endif
+#ifdef OMPTGT
+!$omp target data use_device_addr(a,ev,u,w,dev_info_d,workspace_d)
+#endif
+    cusolver_status = cusolverDnDsyevj &
+        (cusolver_handle, jobz, uplo, ndim,a,ndim,diag, &
+        workspace_d,lwork2,dev_info_d,syevj_params)
+#ifdef ACC
+!$acc end host_data
+#endif
+#ifdef OMPTGT
+!$omp end target data
+#endif
+    cusolver_status=cudaDeviceSynchronize()
+    if(cusolver_status /= CUSOLVER_STATUS_SUCCESS) &
+        write(*,*) 'cusolverDnDsyevj failed',cusolver_status
+  endif
+#endif
+  
+  ! ======== HIPSOLVER =========
+
+#ifdef HIPSOLVER
+  if(jsolver.eq.SOLVER_HIPSOLVER) then
+    ndim=nelecs
+    mdim=mbasel
+  endif
+#endif
+
+  ! ======= HIPSOLVERJ =========
+  
+#ifdef HIPSOLVERJ
+  if(jsolver.eq.SOLVER_HIPSOLVERJ) then
+    ndim=nelecs
+    mdim=mbasel
+  endif
+#endif
+
+  ! ======== ROCSOLVER =========
+  
+#ifdef ROCSOLVER
+  if(jsolver.eq.SOLVER_ROCSOLVER) then
+    ndim=nelecs
+    mdim=mbasel
+  endif
+#endif
+
+  ! ======= ROCSOLVERJ =========
+  
+#ifdef ROCSOLVERJ
+  if(jsolver.eq.SOLVER_ROCSOLVERJ) then
+    ndim=nelecs
+    mdim=mbasel
+  endif
+#endif
+    
+
+  return
+end subroutine gronor_evd
+
+
+
+subroutine gronor_evd_omp(lfndbg)
+  use cidist
+  use gnome_parameters
+  use gnome_data
+  use gnome_solvers
+
+  ! library specific modules
+
+#ifdef MKL
+  use mkl_solver
+#endif
+
+  ! variable declarations
+
+  implicit none
+  
+  integer :: lfndbg
+  integer :: i
+  integer :: ierr
+
+  ! ========== EISPACK =========
+
+  if(jsolver.eq.SOLVER_EISPACK) then
+#ifdef OMP
+!$omp parallel do
+!$omp& shared(sdiag)
+#endif
+    do i=1,nelecs
+      sdiag(i)=0.0d0
+    enddo
+#ifdef OMP
+!$omp end parallel do
+#endif
+    call tred2(nelecs,nelecs,a,nelecs,nelecs,diag, &
+        nelecs,sdiag,nelecs,a,nelecs,nelecs)
+    call tql2(nelecs,nelecs,diag,nelecs,sdiag,nelecs,a,nelecs, &
+        nelecs,ierr)
+  endif
+  
+  ! ============ MKL ===========
+  
+#ifdef MKL
+  if(jsolver.eq.SOLVER_MKL) then
+    ndimm=nelecs
+    call dsyevd('N','L',ndimm,a,nelecs,diag,workspace_d,lwork1m, &
+        workspace_i,lworki,ierr)
+  endif
+#endif 
+
+  return
+end subroutine gronor_evd_omp

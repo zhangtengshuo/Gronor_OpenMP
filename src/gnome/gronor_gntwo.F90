2cf6ee55f345feca5abb1446d34f779d8258e4eb
T. P. Straatsma <mailb@tpstraatsma.com>
2024-01-06 10:58:19 -0500

    Bug Fix: Add missing end kernels

--- src/gnome/gronor_gntwo.F90
+++ src/gnome/gronor_gntwo.F90
@@ -658,6 +658,9 @@ subroutine gronor_gntwo_canonical(lfndbg)
 !$omp end target teams distribute parallel do
 #endif
 #endif
+#ifdef ACC
+!$acc end kernels
+#endif
 
     ldiag=diagmax.gt.1.0e-16
     lbdiag=bdiagmax.gt.1.0e-16

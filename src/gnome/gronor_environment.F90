3af51b9bbca22388b79278e93cc52eb54ae88589
shuo <54305171+zhangtengshuo@users.noreply.github.com>
2025-08-04 17:14:25 +0800

    Abort when MPI_THREAD_MULTIPLE unsupported

--- src/gnome/gronor_environment.F90
+++ src/gnome/gronor_environment.F90
@@ -70,7 +70,8 @@ subroutine gronor_environment()
   call mpi_comm_rank(MPI_COMM_WORLD,me,ierr)
   call mpi_comm_size(MPI_COMM_WORLD,np,ierr)
   if(provided_thread_level < MPI_THREAD_MULTIPLE) then
-    if(me == 0) write(*,*) 'Warning: MPI_THREAD_MULTIPLE not fully supported'
+    if(me == 0) write(*,*) 'Error: MPI_THREAD_MULTIPLE not fully supported'
+    call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
   endif
 
   !     master process is last in the list to enable more effective
